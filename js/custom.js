342f2d78fd73c7a955359dfa79815207a71e76f3
yassermuayed <yassermuayed@gmail.com>
2024-08-19 12:05:06 +0300

    some changes

--- js/custom.js
+++ js/custom.js
@@ -66,10 +66,10 @@ $('.slider_btn_next').on('click', function (e) {
 
 /** google_map js **/
 
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+// function myMap() {
+//     var mapProp = {
+//         center: new google.maps.LatLng(40.712775, -74.005973),
+//         zoom: 18,
+//     };
+//     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+// }
